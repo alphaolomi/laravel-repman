1c97bc453611d47f62c30ebfe8c34833a51b38d1
alphaolomi <alphaolomi@users.noreply.github.com>
2022-08-18 11:16:19 +0000

    Fix styling

--- src/Resources/OrganizationResource.php
+++ src/Resources/OrganizationResource.php
@@ -33,7 +33,7 @@ class OrganizationResource
     /**
      * Create a new organization.
      *
-     * @param string $name
+     * @param  string  $name
      * @return Response
      */
     public function addOrganisation(string $name): Response
